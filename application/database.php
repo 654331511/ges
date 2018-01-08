0c9d23b753f4f8528a29237baced4242a3240db5
654331511 <18629656507@163.com>
2018-01-08 11:49:57 +0800

    1

--- application/database.php
+++ application/database.php
@@ -19,9 +19,9 @@ return [
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'root',
+    'password'       => '1234',
     // 端口
-    'hostport'       => '3306',
+    'hostport'       => '',
     // 连接dsn
     'dsn'            => '',
     // 数据库连接参数
